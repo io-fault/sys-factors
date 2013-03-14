9f31f516a919377d21bb70a7b1b739b9d00ee30c
fault.io <devnull@localhost>
2013-03-14 14:39:43 -0700

    Remove setup.py initialization; working towards setup.py autogen.

--- bin/init.py
+++ bin/init.py
@@ -3,33 +3,10 @@
 ##
 import os.path
 
-SETUP = """
-#!/usr/bin/env python
-##
-# setup.py - .release.xdistutils
-##
-import sys
-
-# distutils data is kept in `{project}.release.xdistutils`
-sys.path.insert(0, '')
-
-sys.dont_write_bytecode = True
-import {project}.release.xdistutils as dist
-defaults = dist.standard_setup_keywords()
-sys.dont_write_bytecode = False
-
-if __name__ == '__main__':
-	try:
-		from setuptools import setup
-	except ImportError:
-		from distutils.core import setup
-	setup(**defaults)
-"""
-
 def ignore(dir, names):
 	return [
 		x for x in names
-		if x.endswith('.pyc') or x.endswith('.pyo')
+		if x.endswith('.pyc') or x.endswith('.pyo') or x == '__pycache__'
 	]
 
 def main(project_name):
@@ -38,8 +15,6 @@ def main(project_name):
 	root = os.path.dirname(skeleton.__file__)
 	root = os.path.realpath(root)
 	shutil.copytree(root, project_name, ignore = ignore)
-	with open('setup.py', 'a') as f:
-		f.write(SETUP.format(project = project_name).strip())
 
 if __name__ == '__main__':
 	import sys
