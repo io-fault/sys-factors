f95ac4ede2a54d8a072af162d7772a3a1f9de1fa
fault.io <devnull@localhost>
2013-04-18 14:27:04 -0700

    Add libpython for identifying crossable lines. (needs some work)

---
+++ libpython.py
@@ -0,0 +1,17 @@
+import ast
+
+def lines(path):
+	"""
+	Return the set of lines that have expressions.
+	"""
+	seq = set()
+	with open(path) as f:
+		a = ast.parse(f.read(), path)
+		for x in ast.walk(a):
+			if hasattr(x, 'lineno'):
+				if x.col_offset != -1:
+					if isinstance(x, (ast.Expr, ast.Str)):
+						# Covers some doc-string edges.
+						continue
+					seq.add(x.lineno)
+	return seq
