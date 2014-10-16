04e134b874eb3aaa6d15c2cd7298eddb1db3cc6c
fault <critical@fault.io>
2014-10-16 16:17:32 -0700

    Docs

---
+++ bin/rattle.py
@@ -0,0 +1,21 @@
+"""
+.bin.rattle - initialize a new project package skeleton
+"""
+import os.path
+
+def ignore(dir, names):
+	return [
+		x for x in names
+		if x.endswith('.pyc') or x.endswith('.pyo') or x == '__pycache__'
+	]
+
+def main(project_name):
+	from .. import skeleton
+	import shutil
+	root = os.path.dirname(skeleton.__file__)
+	root = os.path.realpath(root)
+	shutil.copytree(root, project_name, ignore = ignore)
+
+if __name__ == '__main__':
+	import sys
+	main(sys.argv[1])
