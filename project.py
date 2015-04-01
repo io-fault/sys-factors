40ac2b2ac00b0f3dce4e12a7c84f7f9389db4e51
fault <critical@fault.io>
2015-04-01 15:05:47 -0700

    Remove version data as it relates to releases *of* the project.

--- project.py
+++ project.py
@@ -1,24 +1,17 @@
 'project information'
 
+abstract = 'tools and libraries for supporting a development environment'
+
+identity = 'https://fault.io/project/python/development'
+
 #: Project name.
 name = 'development'
-abstract = 'libraries and tools for Python development'
-icon = '🐜'
+fork = 'industry'
 
-#: IRI based project identity. (project homepage)
-identity = 'https://fault.io/project/development'
+icon = '🐜'
 
 #: Responsible Party
 controller = 'fault.io'
 
 #: Contact point for the Responsible Party
 contact = 'mailto:critical@fault.io'
-
-#: Name of the project's fork; used to identify a distinct development line.
-fork = 'industry'
-
-#: Version tuple: (major, minor, bugfix)
-version_info = (0, 6, 0)
-
-#: The version string.
-version = '.'.join(map(str, version_info))
