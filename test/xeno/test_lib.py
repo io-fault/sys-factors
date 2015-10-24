d289525cbb7ad1ce04c1241e79105eba80665de6
fault <critical@fault.io>
2015-10-24 13:46:17 -0700

    Remove the xeno directory renaming anything salvaged.

--- test/xeno/test_lib.py
+++
@@ -1,5 +0,0 @@
-from .. import lib
-
-if __name__ == '__main__':
-	import sys; from ...development import libtest
-	libtest.execute(sys.modules['__main__'])
--- test/xeno/xprobe/probed.py
+++
@@ -1,7 +0,0 @@
-from ... import libframe, libprobe
-
-# used to validate module identity
-data = 'expected'
-
-def probe(context):
-	context.define(foo = '"bar"')
