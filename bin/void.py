0c8ee22f0b1508f8280d93c3be055a8f503db167
fault <critical@fault.io>
2016-01-27 11:55:15 -0700

    Clean script for packages.

---
+++ bin/void.py
@@ -0,0 +1,26 @@
+"""
+Destroy the (system:directory)`__pycache__` directories in the
+directories given as parameters to the command.
+"""
+
+import sys
+from ...routes import library as libroutes
+
+def descend(route, directory='__pycache__'):
+	p = (route / directory)
+	if p.exists():
+		assert '/__pycache__' in p.fullpath
+		print('removing:', str(p))
+		p.void()
+
+	dirs = route.subnodes()[0]
+	for r in dirs:
+		descend(r)
+
+def main(args, directory='__pycache__', File=libroutes.File):
+	for path in args:
+		fr = File.from_path(path)
+		descend(fr)
+
+if __name__ == '__main__':
+	main(sys.argv[1:])
