aac8beb8118e1c6b16b3acc63bed62dee905f0c3
fault <critical@fault.io>
2016-07-17 15:43:34 -0700

    Add some surface documentation.

--- __init__.py
+++ __init__.py
@@ -1,3 +1,11 @@
 """
+[ Executables ]
+
+/&.bin.prepare
+	Prepare a product for use by compiling bytecode and extension modules.
+/&.bin.test
+	Test a product and its factors providing debugging consoles upon failures.
+/&.bin.validate
+	Perform the tests of a product using the (fault:role)`optimal` role.
 """
 __factor_type__ = 'project'
