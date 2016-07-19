4eb7c4c1485c43721b4f1b64859b566b2793c9e4
fault <critical@fault.io>
2016-07-18 22:41:11 -0700

    Add more executables.

--- __init__.py
+++ __init__.py
@@ -1,11 +1,24 @@
 """
 [ Executables ]
 
+/&.bin.configure
+	Initialize libconstruct directory for performing host compilation.
+	Must be called per-suer.
+/&.bin.iterate
+	Prepare and measure a project storing the structures and formatted HTML pages
+	into a set of &..filesystem.library.Directory instances. Procedure executable
+	running executables from &..development and &..factors.
+/&.bin.log
+	View the construction logs of the specified composite factor (module).
 /&.bin.prepare
 	Prepare a product for use by compiling bytecode and extension modules.
+/&.bin.measure
+	Measure the coverage of a project or context using its tests.
 /&.bin.test
 	Test a product and its factors providing debugging consoles upon failures.
 /&.bin.validate
 	Perform the tests of a product using the (fault:role)`optimal` role.
+/&.bin.void
+	Destroy (filesystem:pattern)`__pycache__` directories in the given path.
 """
 __factor_type__ = 'project'
