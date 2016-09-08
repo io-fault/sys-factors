a5bcfd98878014be7d5e77a5e3ab212cf562a8fa
fault <critical@fault.io>
2016-09-08 10:56:11 -0700

    Use unqualified names

--- project.py
+++ project.py
@@ -1,9 +1,10 @@
-identity = 'http://fault.io/python/fault.development'
+identity = 'http://fault.io/python/development'
 name = 'development'
 abstract = 'Construction, coverage, testing for software development'
 icon = '🐜'
 
 fork = 'bootstraps'
+versioning = 'continuous'
 status = 'flux'
 
 controller = 'fault.io'
