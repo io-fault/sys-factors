abfcd4050e839c987057d110585fefb1f22aba37
fault <critical@fault.io>
2017-05-01 06:28:57 -0700

    Formatting.
    Minor adjustments to runtime indicating path forward.

--- libprobe.py
+++ libprobe.py
@@ -1,8 +1,5 @@
 """
 # System feature probes for executables and the runtime environment.
-
-# &.libprobe provides a set of system queries for detecting executables,
-# compiler collections, headers, and libraries.
 """
 import os
 import subprocess
@@ -14,17 +11,11 @@ import typing
 import types
 
 from . import library as libdev
-libfactor = libdev.libfactor
 
 from ..routes import library as libroutes
 from ..io import library as libio
 
 def fs_routes(i:typing.Iterator[str]) -> typing.Sequence[libroutes.File]:
-	"""
-	# Construct a &list of &libroutes.File instances from the given
-	# iterator of strings.
-	"""
-	global libroutes
 	return list(map(libroutes.File.from_absolute, i))
 
 def environ_paths(env='PATH', sep=os.pathsep):
@@ -46,15 +37,15 @@ def environ_paths(env='PATH', sep=os.pathsep):
 	# /sep
 		# The path separator to split the environment variable on.
 	"""
-	global os
-	global fs_routes
 
 	s = os.environ[env].split(sep)
 	seq = fs_routes(s)
 
 	return seq
 
-def search(search_paths:typing.Sequence[str], xset:typing.Set[str]
+def search(
+		search_paths:typing.Sequence[str],
+		xset:typing.Set[str]
 	) -> typing.Tuple[typing.Mapping[str, libroutes.File], typing.Set[str]]:
 	"""
 	# Query the sequence of search paths for the given set of files.
@@ -90,7 +81,6 @@ def executables(xset:typing.Set[str]):
 	# Returns a pair whose first item is the matches that currently exist,
 	# and the second is the set of executables that were not found in the path.
 	"""
-	global search, environ_paths
 	return search(environ_paths(), xset)
 
 def prepare(
@@ -100,7 +90,6 @@ def prepare(
 		libraries:typing.Sequence[str]=(),
 		library_directories:typing.Sequence[str]=(),
 		include_directories:typing.Sequence[str]=(),
-		preprocessor:typing.Sequence[str]=(),
 		compile_only:bool=False,
 		lmap={
 			'c': 'c',
@@ -121,7 +110,7 @@ def prepare(
 
 	output = None
 
-	r = libroutes.Import.from_fullname('fault.development.probe') # fake composite.
+	r = libroutes.Import.from_fullname('fault.development.probes') # fake composite.
 
 	src = directory/'src'
 	exe = directory/'fault_probe_runtime_check.exe'
@@ -136,10 +125,8 @@ def prepare(
 	mod.generated = 'probe'
 
 	mod.system = {
-		'source.parameters': list(preprocessor),
-		'library.set': set(libraries),
-		'library.directories': set(library_directories),
-		'include.directories': list(include_directories),
+		('system', 'library'): set(libraries),
+		('source', 'library'): set(include_directories),
 	}
 
 	f = libdev.Factor(r, mod, None)
@@ -184,7 +171,7 @@ def sysctl(names, route=None):
 	"""
 	# Retrieve the system control variables using (system:executable)`sysctl`.
 
-	# [Parameters]
+	# [ Parameters ]
 	# /route
 		# The route to the `sysctl` executable.
 	# /names
@@ -225,6 +212,13 @@ def includes(
 		# A sequence of includes to test for. A sequence is used so that
 		# dependencies may be included prior to the actual header or headers of interest.
 		# The check is only interested in whether or not compilation succeeded.
+
+	# [ Returns ]
+	# /&True
+		# All headers were included in the Translation Unit without error.
+	# /&False
+		# The compilation of the Translation Unit resulted in an error,
+		# indicating the absence of the headers or a misconfiguration.
 	"""
 
 	main = "\nint main(int argc, char *argv[]) { return 0; }"
