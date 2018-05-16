babdbe6f6f6edcc717cc2408ed2e7f8ba6a88f82
fault <critical@fault.io>
2018-05-16 00:13:53 -0700

    Destroy f_cache as well

--- bin/void.py
+++ bin/void.py
@@ -1,17 +1,19 @@
 """
-# Destroy the (system:directory)`__pycache__` directories in the
+# Destroy the (system/filename)`__pycache__` (system/filename)`__f_cache__` directories in the
 # directories given as parameters to the command.
 """
 
 import sys
 from fault.routes import library as libroutes
 
-def descend(route, directory='__pycache__'):
-	p = (route / directory)
-	if p.exists():
-		assert '/__pycache__' in p.fullpath
-		print('removing:', str(p))
-		p.void()
+def descend(route, directories={'__pycache__', '__f_cache__'}):
+	for x in directories:
+		p = (route / x)
+		if p.exists():
+			path = str(p)
+			assert path.endswith('/__pycache__') or path.endswith('__f_cache__')
+			print('removing:', str(p))
+			p.void()
 
 	dirs = route.subnodes()[0]
 	for r in dirs:
