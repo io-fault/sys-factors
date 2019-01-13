00dd6fb388e9efef2533341b395a836bcbc793f3
fault <critical@fault.io>
2019-01-13 12:30:40 -0700

    Initial implementation for source [data] profiles.

--- bin/void.py
+++ bin/void.py
@@ -1,12 +1,53 @@
 """
-# Perform nothing and note that the factor type was not supported by the context.
+# Construct a data profile file describing the contents of the source files.
 """
 import sys
+import hashlib
+import collections
+
 from fault.system import process
+from fault.system import files
+
+def measure_source(route):
+	lc = 0
+	d = b''
+	size = 0
+	h = hashlib.new('sha256', b'')
+	depth = collections.Counter()
+
+	with open(str(route), 'rb') as f:
+		continued = True
+		while continued:
+			buf = f.read(2048)
+			size += len(buf)
+			h.update(buf)
+
+			if not buf:
+				continued = False
+				lc += 1
+			else:
+				d += buf
+				lines = d.split(b'\n')
+				for x in lines:
+					il = len(x) - len(x.lstrip(b'\t'))
+					depth[il] += 1
+
+				lc += len(lines) - 1
+				d = lines[-1]
+
+	return size, h, lc
 
 def main(inv:process.Invocation) -> process.Exit:
-	sys.stderr.write("unsupported factor type\n")
-	return inv.exit(101)
+	out, *inf = map(files.Path.from_path, inv.args)
+	template = "[%s]\nsize: %d\nsha1_256: %s\nlines: %d\n"
+
+	outlines = []
+	for r in inf:
+		size, hash, lc = measure_source(r)
+		outlines.append(template % (str(r), size, hash.hexdigest(), lc))
+
+	out.set_text_content('\n'.join(outlines))
+	return inv.exit(0)
 
 if __name__ == '__main__':
 	process.control(main, process.Invocation.system())
