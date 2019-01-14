a23cf4fda5032384e19f01f5fbf552e6ce3daeaf
fault <critical@fault.io>
2019-01-14 00:22:28 -0700

    Eliminate some bitrot; most of the tests were for the old implementation

---
+++ test/test_cc.py
@@ -0,0 +1,52 @@
+"""
+# Construction Context tests.
+"""
+from fault.routes import library as libroutes
+from fault.system import files
+from fault.system import python
+
+from .. import cc as module
+
+def test_Factor(test):
+	"""
+	# Check the Construction Factor features.
+	"""
+	f = module.Factor.from_fullname(__name__)
+	test.isinstance(f.module, types.ModuleType)
+	test.isinstance(f.route, python.Import)
+	test.isinstance(f.module_file, files.Path)
+
+	# Check cache directory.
+	pkgdir = os.path.dirname(__file__)
+	pkgcache = os.path.join(pkgdir, '__pycache__')
+	test/str(f.cache_directory) == pkgcache
+
+	# Defaults.
+	test/f.domain == 'python'
+	test/f.type == 'library'
+	test/str(f.source_directory) == str(pkgdir)
+
+	fpir = os.path.join(pkgcache, module.Factor.default_fpi_name)
+	test/str(f.fpi_root) == str(fpir)
+
+def test_updated(test):
+	tr = test.exits.enter_context(files.Path.temporary())
+
+	of = tr / 'obj'
+	sf = tr / 'src'
+	test/module.updated([of], [sf], None) == False
+
+	# object older than source
+	of.init('file')
+	sf.init('file')
+	of.set_last_modified(sf.get_last_modified().rollback(second=10))
+	test/module.updated([of], [sf], None) == False
+
+	of.void()
+	of.init('file')
+	of.set_last_modified(sf.get_last_modified().elapse(second=10))
+	test/module.updated([of], [sf], None) == True
+
+if __name__ == '__main__':
+	from fault.test import library as libtest; import sys
+	libtest.execute(sys.modules[__name__])
