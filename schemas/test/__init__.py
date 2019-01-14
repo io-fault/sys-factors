48b1939819f278f852bad733850f2185a98c2ae8
fault <critical@fault.io>
2019-01-13 21:53:26 -0700

    Remove unused XML schemas

--- schemas/test/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-# Test report rendering and validation.
-"""
-from .. import traceback
-__factor_domain__ = 'xml'
-__factor_type__ = 'executable'
--- schemas/traceback/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-# Traceback serialization.
-"""
-__factor_domain__ = 'xml'
-__factor_type__ = 'executable'
