bbdd6738c560c407e3ff2dcc40fe37c511aca8ca
fault <critical@fault.io>
2020-02-23 20:44:26 -0700

    Bitrot

--- test/test_cc.py
+++ test/test_cc.py
@@ -2,31 +2,7 @@
 # Construction Context tests.
 """
 from fault.system import files
-from fault.system import python
-
 from .. import cc as module
-
-def test_Factor(test):
-	"""
-	# Check the Construction Factor features.
-	"""
-	f = module.Factor.from_fullname(__name__)
-	test.isinstance(f.module, types.ModuleType)
-	test.isinstance(f.route, python.Import)
-	test.isinstance(f.module_file, files.Path)
-
-	# Check cache directory.
-	pkgdir = os.path.dirname(__file__)
-	pkgcache = os.path.join(pkgdir, '__pycache__')
-	test/str(f.cache_directory) == pkgcache
-
-	# Defaults.
-	test/f.domain == 'python'
-	test/f.type == 'library'
-	test/str(f.source_directory) == str(pkgdir)
-
-	fpir = os.path.join(pkgcache, module.Factor.default_fpi_name)
-	test/str(f.fpi_root) == str(fpir)
 
 def test_updated(test):
 	tr = test.exits.enter_context(files.Path.temporary())
